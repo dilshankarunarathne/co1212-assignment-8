9d55322599befa6638c306e22fc0ff4be196fa14
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-01 11:31:12 +0530

    Update Main.java

--- src/me/karunarathne/Palindrome/Main.java
+++ src/me/karunarathne/Palindrome/Main.java
@@ -2,7 +2,7 @@ package me.karunarathne.Palindrome;
 
 public class Main {
     public static void main(String[] args) {
-        String word = "alpoyologo";
+        String word = "madam";
         StackChecker.checkIfPalindrome(word);
     }
 }
