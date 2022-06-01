db9f4cb82d88c387aee4f956591738d17d5a0b14
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-01 11:45:59 +0530

    Update Queue.java

--- src/me/karunarathne/Palindrome/Queue.java
+++ src/me/karunarathne/Palindrome/Queue.java
@@ -1,4 +1,105 @@
 package me.karunarathne.Palindrome;
 
+import java.util.Collection;
+import java.util.Iterator;
+
 public class Queue implements java.util.Queue {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /**
+     * not needed
+     * @param o element whose presence in this collection is to be tested
+     * @return
+     */
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    /**
+     * not using here, but useful
+     * @return
+     */
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean add(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean offer(Object o) {
+        return false;
+    }
+
+    @Override
+    public Object remove() {
+        return null;
+    }
+
+    @Override
+    public Object poll() {
+        return null;
+    }
+
+    @Override
+    public Object element() {
+        return null;
+    }
+
+    @Override
+    public Object peek() {
+        return null;
+    }
 }
