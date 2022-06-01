97a7f4820b4700a262cb606d5c3add212a64aae5
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-01 11:33:51 +0530

    Update StackChecker.java

--- src/me/karunarathne/Palindrome/StackChecker.java
+++ src/me/karunarathne/Palindrome/StackChecker.java
@@ -34,10 +34,11 @@ public class StackChecker {
     }
 
     private static boolean checkStack(String word) {
+        String slice;
         if (length % 2 == 1) {  // odd    TODO check again
             fillStack(word, length/2);
         } else {                // even
-            fillStack(word, length/2);
+            slice = word.substring(length/2, length);
         }
         String slice = word.substring(length/2, length);
 
