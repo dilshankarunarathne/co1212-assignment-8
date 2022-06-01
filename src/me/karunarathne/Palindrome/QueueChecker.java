acfd5b07c59abe74a0e3824168079988c52ff29a
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-01 12:17:20 +0530

    Update QueueChecker.java

--- src/me/karunarathne/Palindrome/QueueChecker.java
+++ src/me/karunarathne/Palindrome/QueueChecker.java
@@ -8,10 +8,11 @@ public class QueueChecker {
     public static boolean isPalindrome(String word) {
         charQueue = new CharQueue(word.substring(word.length()/2));
 
+        printQueue();
         return false;
     }
 
-    private static void printStack() {
+    private static void printQueue() {
         charQueue.forEach(
                 i -> System.out.println((char) i)
         );
