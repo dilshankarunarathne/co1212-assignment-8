7ce2e2f4edc5f4cbb4e26fc4db37e773f6c689c5
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-01 12:42:36 +0530

    Update Main.java

--- src/me/karunarathne/Palindrome/Main.java
+++ src/me/karunarathne/Palindrome/Main.java
@@ -2,10 +2,13 @@ package me.karunarathne.Palindrome;
 
 public class Main {
     public static void main(String[] args) {
+        stackTests();
         queueTests();
     }
 
     private static void queueTests() {
+        System.out.println("------------------------------");
+        System.out.println("         Q1-Stack");
         QueueChecker.checkIfPalindrome("srilanka");
         QueueChecker.checkIfPalindrome("madam");
         QueueChecker.checkIfPalindrome("hannah");
