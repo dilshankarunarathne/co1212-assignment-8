32c1163a275815165449c193378589348f816abc
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-01 12:06:45 +0530

    Update CharQueue.java

--- src/me/karunarathne/Palindrome/CharQueue.java
+++ src/me/karunarathne/Palindrome/CharQueue.java
@@ -21,7 +21,7 @@ public class CharQueue implements Queue {
      */
     @Override
     public boolean add(Object o) {
-        charArray.add((Character) o);
+        return charArray.add((Character) o);
     }
 
     /**
