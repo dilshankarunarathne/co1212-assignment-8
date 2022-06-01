ca161dbb26330f303c8d26b90299271369d66308
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-01 14:29:50 +0530

    Update Main.java

--- src/me/karunarathne/Palindrome/Main.java
+++ src/me/karunarathne/Palindrome/Main.java
@@ -8,29 +8,29 @@ public class Main {
     }
 
     private static void stackTests() {
-        System.out.println("------------------------------");
         System.out.println("         Q1 - Stack");
         StackChecker.checkIfPalindrome("srilanka");
         StackChecker.checkIfPalindrome("madam");
         StackChecker.checkIfPalindrome("hannah");
         StackChecker.checkIfPalindrome("rocketman");
+        System.out.println("------------------------------");
     }
 
     private static void queueTests() {
-        System.out.println("------------------------------");
         System.out.println("         Q2 - Queue");
         QueueChecker.checkIfPalindrome("srilanka");
         QueueChecker.checkIfPalindrome("madam");
         QueueChecker.checkIfPalindrome("hannah");
         QueueChecker.checkIfPalindrome("rocketman");
+        System.out.println("------------------------------");
     }
 
     private static void stackNQueueTests() {
-        System.out.println("------------------------------");
         System.out.println("      Q3 - Stack & Queue");
         StackNQueueChecker.checkIfPalindrome("srilanka");
         StackNQueueChecker.checkIfPalindrome("madam");
         StackNQueueChecker.checkIfPalindrome("hannah");
         StackNQueueChecker.checkIfPalindrome("rocketman");
+        System.out.println("------------------------------");
     }
 }
