c8c3958f7b3c29b232077ae4b36e1ea6c0e84f9c
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-01 08:06:56 +0530

    Update Queue.java

--- src/com/esn/eusl/CO1212/Queue.java
+++ src/com/esn/eusl/CO1212/Queue.java
@@ -1,80 +1,62 @@
-public class Queue
-{
-	int size = 5;
-	int myQueue[] = new int[size];
-	int front, rear;
+public class Queue {
+    int size = 5;
+    int myQueue[] = new int[size];
+    int front, rear;
 
-	Queue()
-	{
-		front = rear = -1;
-	}
+    Queue() {
+        front = rear = -1;
+    }
 
-	boolean isFull()
-	{
-		if (front == 0 && rear == size - 1)
-		{
-			return true;
-		}
-		return false;
-	}
+    boolean isFull() {
+        if (front == 0 && rear == size - 1) {
+            return true;
+        }
+        return false;
+    }
 
-	boolean isEmpty()
-	{
-		if (front == -1)
-			return true;
-		else
-			return false;
-	}
+    boolean isEmpty() {
+        if (front == -1)
+            return true;
+        else
+            return false;
+    }
 
-	void enQueue(int element)
-	{
-		if (isFull())
-		{
-			System.out.println("Queue is full");
-		}
-		else
-		{
-			if (front == -1)
-			{
-				front = 0;
-			}
-			rear++;
-			myQueue[rear] = element;
-			System.out.println("Insert " + element);
-		}
-	}
+    void enQueue(int element) {
+        if (isFull()) {
+            System.out.println("Queue is full");
+        } else {
+            if (front == -1) {
+                front = 0;
+            }
+            rear++;
+            myQueue[rear] = element;
+            System.out.println("Insert " + element);
+        }
+    }
 
-	int deQueue()
-	{
-		int element;
-		if (isEmpty())
-		{
-			System.out.println("Queue is empty");
-			return (-1);
-		}
-		else
-		{
-			element = myQueue[front];
-			if (front >= rear)
-				front = rear = -1;
-			else
-				front++;
-			System.out.println( element + " is deleted");
-			return (element);
-		}
-	}
+    int deQueue() {
+        int element;
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return (-1);
+        } else {
+            element = myQueue[front];
+            if (front >= rear)
+                front = rear = -1;
+            else
+                front++;
+            System.out.println(element + " is deleted");
+            return (element);
+        }
+    }
 
-	void display()
-	{
-		if (isEmpty())
-		{
-			System.out.println("Empty Queue");
-		}
-		else
-		{
-			for (int i = front; i <= rear; i++)
-				System.out.print(myQueue[i] + "  ");
-		}
-		System.out.println();
-	}
+    void display() {
+        if (isEmpty()) {
+            System.out.println("Empty Queue");
+        } else {
+            for (int i = front; i <= rear; i++)
+                System.out.print(myQueue[i] + "  ");
+        }
+        System.out.println();
+    }
 }
