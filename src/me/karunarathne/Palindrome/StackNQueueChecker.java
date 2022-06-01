51c89b187c8593d796f352108e1f9571113e8af4
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-01 14:03:45 +0530

    Update StackNQueueChecker.java

--- src/me/karunarathne/Palindrome/StackNQueueChecker.java
+++ src/me/karunarathne/Palindrome/StackNQueueChecker.java
@@ -1,6 +1,8 @@
 package me.karunarathne.Palindrome;
 
 public class StackNQueueChecker {
+    private static Stack<Character> charStack;
+    
     public static boolean checkIfPalindrome(String word) {
     }
 }
