b8ced7431f420ab5b079b163d622d0e5d7afbeaf
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-04 20:36:49 +0530

    Update Q2.java

--- src/me/karunarathne/Palindrome/Basica/Q2.java
+++ src/me/karunarathne/Palindrome/Basica/Q2.java
@@ -1,4 +1,10 @@
 package me.karunarathne.Palindrome.Basica;
 
+import java.util.Scanner;
+
 public class Q2 {
+    private static CharStack stack ;
+    private static Scanner scanner ;
+    private static String inputString ;
+    private static int strLen ;
 }
