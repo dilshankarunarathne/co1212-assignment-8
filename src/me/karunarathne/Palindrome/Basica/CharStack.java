a23e1b193e3bcdae95ce6b3bc5ec9a66dcd5cc57
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-04 19:37:55 +0530

    Update CharStack.java

--- src/me/karunarathne/Palindrome/Basica/CharStack.java
+++ src/me/karunarathne/Palindrome/Basica/CharStack.java
@@ -2,8 +2,15 @@ package me.karunarathne.Palindrome.Basica;
 
 public class CharStack {
     private Node genesisNode;
+    private int size;
 
-    public void push(char)
+    public CharStack() {
+        size = 0;
+    }
+
+    public void push(char character) {
+        if
+    }
     private class Node {
         char head;
         Node next;
