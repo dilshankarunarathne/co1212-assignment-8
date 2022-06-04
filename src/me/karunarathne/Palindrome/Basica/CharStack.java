bfabd2b35ba385ef481e7f531c72a2c46ed650a0
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-04 19:41:00 +0530

    ws

--- src/me/karunarathne/Palindrome/Basica/CharStack.java
+++ src/me/karunarathne/Palindrome/Basica/CharStack.java
@@ -12,16 +12,22 @@ public class CharStack {
         if (size == 0) {
             headNode = new Node(character);
         } else {
-
+            headNode = new Node(character, headNode);
         }
         size ++ ;
     }
     private class Node {
         char head;
         Node next;
+        Node prev;
 
         public Node(char head) {
             this.head = head;
+        }
+
+        public Node(char head, Node prev) {
+            this.head = head;
+            this.prev = prev;
         }
     }
 }
