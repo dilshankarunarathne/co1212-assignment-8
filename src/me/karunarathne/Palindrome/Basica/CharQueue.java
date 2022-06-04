a85b2ff3610776424e6ae65c0fffda0303e2064a
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-04 20:22:48 +0530

    Update CharQueue.java

--- src/me/karunarathne/Palindrome/Basica/CharQueue.java
+++ src/me/karunarathne/Palindrome/Basica/CharQueue.java
@@ -1,4 +1,22 @@
 package me.karunarathne.Palindrome.Basica;
 
 public class CharQueue {
+    private CharStack.Node headNode;
+    private int size;
+    
+    private static class Node {
+        char data;
+        Node next;
+        Node prev;
+
+        public Node(char data) {
+            this.data = data;
+        }
+
+        public Node(char data, Node prev) {
+            this.data = data;
+            this.prev = prev;
+            prev.next = this;
+        }
+    }
 }
