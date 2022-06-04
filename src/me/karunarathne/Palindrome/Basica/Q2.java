03cb42217ccbe85467debb574a092599deccbe11
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-04 20:37:17 +0530

    Update Q2.java

--- src/me/karunarathne/Palindrome/Basica/Q2.java
+++ src/me/karunarathne/Palindrome/Basica/Q2.java
@@ -7,4 +7,12 @@ public class Q2 {
     private static Scanner scanner ;
     private static String inputString ;
     private static int strLen ;
+
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+        stack = new CharStack();
+        populateStack(readInput());
+        strLen = inputString.length();
+        printResult(checkIfPalindrome());
+    }
 }
