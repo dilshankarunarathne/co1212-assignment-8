69a54c45fc17001b9dd59ee072e287dfba142e4d
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-04 20:57:18 +0530

    Update Q3.java

--- src/me/karunarathne/Palindrome/Basica/Q3.java
+++ src/me/karunarathne/Palindrome/Basica/Q3.java
@@ -7,12 +7,17 @@ public class Q3 {
     private static CharQueue queue;
     private static String inputString;
     private static Scanner scanner;
+    private static int strLen;
 
     public static void main(String[] args) {
         scanner = new Scanner(System.in);
         queue = new CharQueue();
+
         populateStack(readInput());
         strLen = inputString.length();
+
+
+
         printResult(checkIfPalindrome());
     }
 }
